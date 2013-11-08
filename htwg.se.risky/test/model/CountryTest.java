03acc9b293a18e07b386889e7e5289c5c2573199
lustoppe <lustoppe@3DLAB12.in.fhkn.de>
2013-11-08 15:29:42 +0100

    kein änderung.

--- htwg.se.risky/test/model/CountryTest.java
+++ htwg.se.risky/test/model/CountryTest.java
@@ -2,9 +2,9 @@ package model;
 
 import static org.junit.Assert.*;
 
-import org.junit.Rule;
+//import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+//import org.junit.rules.ExpectedException;
 
 public class CountryTest {
 	
